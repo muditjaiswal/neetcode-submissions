be39022abcb8290598e402094d9e350c16b1ee94
Mudit Jaiswal <46639508+muditjaiswal@users.noreply.github.com>
2026-08-08 15:55:41 -0700

    Add: python-stack-push-pop - submission-1

---
+++ Python Coding Interviews/python-stack-push-pop/submission-1.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+def reverse_list(arr: List[int]) -> List[int]:
+    stack =[]
+    while arr :
+        stack.append(arr.pop())
+    return stack
+
+    
+
+
+# do not modify below this line
+print(reverse_list([1, 2, 3]))
+print(reverse_list([3, 2, 1, 4, 6, 2]))
+print(reverse_list([1, 9, 7, 3, 2, 1, 4, 6, 2]))
